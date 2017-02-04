c7641e02f1c58d628a20dbda9885fa5a6e44c355
biju1012 <antityp@gmx.de>
2017-02-04 17:18:36 +0100

    Kleiner Textänderung

--- app/src/main/java/com/example/internet/helloworld/MainActivity.java
+++ app/src/main/java/com/example/internet/helloworld/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with my own !action!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with my own !aktion!", Snackbar.LENGTH_LONG)
+                        .setAction("Aktion", null).show();
             }
         });
     }
