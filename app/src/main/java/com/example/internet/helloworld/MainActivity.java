3aa20c62b25e69d2f0d4a9271243d64046279212
biju1012 <antityp@gmx.de>
2017-02-04 19:23:54 +0100

    Arbeit an Second Activity

--- app/src/main/java/com/example/internet/helloworld/MainActivity.java
+++ app/src/main/java/com/example/internet/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button button = (Button) findViewById(R.id.bt_first_to_second);
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -48,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void goToSecondActivity () {
+
     }
 }
---
+++ app/src/main/java/com/example/internet/helloworld/SecondActivity.java
@@ -0,0 +1,7 @@
+package com.example.internet.helloworld;
+
+/**
+ * Created by Internet on 04.02.2017.
+ */
+public class SecondActivity {
+}
